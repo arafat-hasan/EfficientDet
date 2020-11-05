5a8c183d409b5b30d8f26cb1d49e138d23fd0ea3
Arafat Hasan <opendoor.arafat@gmail.com>
2020-11-05 21:37:04 +0600

    var fix

--- train.py
+++ train.py
@@ -107,7 +107,7 @@ def create_callbacks(training_model, prediction_model, validation_generator, arg
         checkpoint = keras.callbacks.ModelCheckpoint(
             os.path.join(
                 args.snapshot_path,
-                f'{args.dataset_type}_{{epoch+start_epoch:02d}}_{{loss:.4f}}_{{val_loss:.4f}}.h5' if args.compute_val_loss
+                f'{args.dataset_type}_{{epoch+args.start_epoch:02d}}_{{loss:.4f}}_{{val_loss:.4f}}.h5' if args.compute_val_loss
                 else f'{args.dataset_type}_{{epoch:02d}}_{{loss:.4f}}.h5'
             ),
             verbose=1,
